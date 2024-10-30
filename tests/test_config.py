c3846add4daa8566ed42153a77ed79f396a2b18a
yamaimo <hello@yamaimo.dev>
2024-10-31 01:19:45 +0900

    Add unit test for config values property.

--- tests/test_config.py
+++ tests/test_config.py
@@ -61,3 +61,10 @@ class TestConfig(TestCase):
         config = Config({"item1": 1, "item2": "test"})
         with self.assertRaises(RuntimeError):
             config.get("item0")
+
+    def test_values(self) -> None:
+        config = Config({"item1": 1, "item2": "test"})
+        values = config.values
+
+        expected = {"item1": 1, "item2": "test"}
+        self.assertDictEqual(values, expected)
